22d0730405e13f9001382fb97ed52a3f052d8828
Nick Satriano <nicksach2001@gmail.com>
2023-07-06 17:17:44 -0400

    Adding 404.html to help with refresh issues

--- src/components/Portfolio/projectItem.js
+++ src/components/Portfolio/projectItem.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 function ProjectItem({ image, name, id }) {
     const navigate = useNavigate();
     return (
-        <div className="projectItem" onClick={() => {navigate("/project/" + id);}}>
+        <div className="projectItem" onClick={() => {navigate("/react_portfolio/project/" + id);}}>
             <div style={{ backgroundImage: `url(${image})` }} className="bgImage" />
             <h2> {name} </h2>
         </div>
