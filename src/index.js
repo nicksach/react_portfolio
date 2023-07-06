6246fcb656e0d2b1fba035b3f04dc4566841cd34
Nick Satriano <nicksach2001@gmail.com>
2023-07-06 17:20:37 -0400

    Trying to fix browser issues

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react_portfolio">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
     ,
