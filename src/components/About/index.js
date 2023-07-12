20635fd39af1276b356803368fd5fa8f5bc290e9
Nick Satriano <nicksach2001@gmail.com>
2023-07-12 13:16:52 -0400

    Updating skills and resume

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -15,12 +15,12 @@ const About = () => {
 
                 <p>
                 I am a programmer with internship experience in Front-End development - though my true passion is for data. 
-                I have been programming since my senior year of high school, and I'm proficient in Python, R, HTML, CSS, Java, C, and Javascript. 
+                I have been programming since my senior year of high school, and I'm proficient in Python, R, HTML, CSS, Java, C, Javascript, and SQL. 
                 I recently graduated from Bucknell University, earning a Bachelor of Science degree in Computer Science.
                 </p>
                     
                 <p>
-                I am actively seeking Python development opportunities, and I am most interested in applying my skills to data-oriented challenges. 
+                I am actively seeking Software Development opportunities, and I am most interested in applying my skills to data-oriented challenges. 
                 I have educational experience performing data analysis and modeling with Pandas, Seaborn, Sci-Kit Learn, TensorFlow, and Keras.
                 </p>
                     
